f8abce587ce911fd37fd31f2e24e90a4c8159144
GustavoGMachado <gustavogmachado2002@gmail.com>
2022-08-11 16:42:45 -0300

    form done

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,6 +17,7 @@ class VerificationEmployee extends Employee {
     constructor(name, lastName, age, cpf, countryOrigin, profissionalArea, workArea, listsCharge, performanceLevel) {
         super(name, lastName, age, cpf, countryOrigin, profissionalArea) 
 
+        this.department = "Verification"
         this.workArea = workArea
         this.listsCharge = listsCharge
         this.performanceLevel = performanceLevel
@@ -27,6 +28,7 @@ class TiEmployee extends Employee {
     constructor(name, lastName, age, cpf, countryOrigin, profissionalArea, developmentArea, hardSkills, seniority) {
         super(name, lastName, age, cpf, countryOrigin, profissionalArea) 
         
+        this.department = "informationTechnology"
         this.developmentArea = developmentArea
         this.hardSkills = hardSkills
         this.seniority = seniority
@@ -37,6 +39,7 @@ class supportEmployee extends Employee {
     constructor(name, lastName, age, cpf, countryOrigin, profissionalArea, knowledgeArea, regionServes, languages) {
         super(name, lastName, age, cpf, countryOrigin, profissionalArea) 
         
+        this.department = "Support"
         this.knowledgeArea = knowledgeArea
         this.regionServes = regionServes
         this.languages = languages
@@ -44,22 +47,34 @@ class supportEmployee extends Employee {
 }
 
 function createVerificationEmployee() {
-    const verGeneralData = captureGeneralData() // Array com os valores nas inputs é retornado
-    const newVerEmployee = new VerificationEmployee(...verGeneralData)
-    manageReg(newVerEmployee)
+    const verGeneralData = captureGeneralData() // Array com os valores nas inputs é retornado ou um "false" caso tiver algum dado inputado incorretamente
+    if (!verGeneralData) {
+        return
+    } else {
+        const newVerEmployee = new VerificationEmployee(...verGeneralData)
+        manageReg(newVerEmployee)
+    }
 }
 function createTiEmployee() {
-    const tiGeneralData = captureGeneralData() // Array com os valores nas inputs é retornado
-    const newTiEmployee = new TiEmployee(...tiGeneralData)
-    manageReg(newTiEmployee)
+    const tiGeneralData = captureGeneralData() // Array com os valores nas inputs é retornado ou um "false" caso tiver algum dado inputado incorretamente
+    if (!tiGeneralData) {
+        return
+    } else {
+        const newTiEmployee = new TiEmployee(...tiGeneralData)
+        manageReg(newTiEmployee)
+    }
 }
 function createSupportEmployee() {
-    const supGeneralData = captureGeneralData() // Array com os valores nas inputs é retornado
-    const newSupEmployee = new supportEmployee(...supGeneralData)
-    manageReg(newSupEmployee)
+    const supGeneralData = captureGeneralData() // Array com os valores nas inputs é retornado ou um "false" caso tiver algum dado inputado incorretamente
+    if (!supGeneralData) {
+        return
+    } else {
+        const newSupEmployee = new supportEmployee(...supGeneralData)
+        manageReg(newSupEmployee)
+    }
 }
 
-// capturando os dados do form e apagando as mensagens de erro
+// apagando as span de erro; capturando os dados do form e testando se estão corretos 
 function captureGeneralData() {
     document.querySelectorAll('.formRegistrationSpan').forEach((el, key) => {
         el.remove()
@@ -67,33 +82,54 @@ function captureGeneralData() {
 
     const generalData = []
 
-    document.querySelectorAll("#formRegistration div input").forEach((el, key) => {
-        verficationData(el, key)
-        generalData.push(el.value)
+    // caso a input esteja correta, o valor da input vai para gerenalData, caso tenha alguma input errada, false vai para o gerenalData
+    const formFields = document.querySelectorAll("#formRegistration div input")
+    formFields.forEach((el, key) => {
+        const dataExists = verficationData(el, key)
+        if (!dataExists) {
+            generalData.push(false)
+        } else {
+            generalData.push(el.value)
+        }
     })
-
-    return generalData
-}
-
-//lançador de erros
-function verficationData(el, key) {
-    if (!el.value) throwErrorData(el, `Campo ${el.placeholder.toUpperCase()} não pode estar vazio, tente novamente!`)
-    console.log(el.placeholder)
-    if ((key === 0 || key === 1 || key === 4)) {
-        const regexLetters = /^[a-záàâãéèêíïóôõöúçñ]+$/i
-        if (regexLetters === false) throwErrorData(el, `Campo ${el.placeholder.toUpperCase()} detectou caracteres inválidos, tente novamente!`)
+    // caso houver algum false no array - algum dado errado descoberto no forEach acima - every retorna false. Se TODOS os dados estejam corretos, every retorna true.
+    const everyGeneralData = generalData.every((el, key) => {
+        if (el === false) {
+            return false 
+        } else {
+            return true
+        }
+    })
+    // retorna false ou o Array com os valores corretos para a Construtora
+    if (!everyGeneralData) {
+        return false
+    } else {
+        cleanForm(formFields)
+        return generalData
     }
-    if ((key === 2 || key === 3) && !Number(el.value)) throwErrorData(el, `Campo ${el.placeholder.toUpperCase()} reconhece apenas números, tente novamente!`)
-    if (key === 3 && el.value.length !== 11) throwErrorData(el, `Campo ${el.placeholder.toUpperCase()} não possui a quantidade de caracteres necessária, tente novamente!`)
-
 }
 
-// erro de campos vazios
-function throwErrorData(el, msg) {
-    let newSpan = document.createElement("span")
-    newSpan.innerHTML = msg
-    newSpan.classList.add("formRegistrationSpan")
-    el.insertAdjacentElement('afterend', newSpan)
+//lançador de erros - verifica e dispara a mensagem de erro na verificação
+function verficationData(el, key) {
+    try {
+        if (!el.value) throw new Error(`Campo ${el.placeholder.toUpperCase()} não pode estar vazio, tente novamente!`)
+        if ((key === 0 || key === 1 || key === 4)) {    
+            const regexLetters = /^[a-záàâãéèêíïóôõöúçñ ]+$/i
+            if (el.value.search(regexLetters) < 0) throw new Error(`Campo ${el.placeholder.toUpperCase()} detectou caracteres inválidos, tente novamente!`)
+        }
+        if ((key === 2 || key === 3) && !Number(el.value)) throw new Error(`Campo ${el.placeholder.toUpperCase()} reconhece apenas números, tente novamente!`)
+        if (key === 3 && el.value.length !== 11) throw new Error(`Campo ${el.placeholder.toUpperCase()} não possui a quantidade de caracteres necessária, tente novamente!`)
+
+    } catch (e) {
+        let newSpan = document.createElement("span")
+        newSpan.innerHTML = e.message
+        newSpan.classList.add("formRegistrationSpan")
+        el.insertAdjacentElement('afterend', newSpan)
+
+        return false
+    }
+
+    return true
 }
 
 // array de cadastrados
@@ -122,15 +158,12 @@ document.querySelector("#cancelForm").addEventListener('click', e => {
     }, 500)
 })
 
-
-
-
-
-
-
-
-
-
+function cleanForm(formFields) {
+    window.alert("Funcionário cadastrado com sucesso!")
+    formFields.forEach((el, key) => {
+        el.value = ''
+    })
+}
 
 // NOME
 // SOBRENOME
